0c652e81de2aab4115a8be2657d0206e712c0990
Sol Kim <defreturnnone@gmail.com>
2017-05-09 12:37:34 +0900

    do not install opensans

--- pyblish_lite/app.py
+++ pyblish_lite/app.py
@@ -28,9 +28,7 @@ def application():
 def install_fonts():
     database = QtGui.QFontDatabase()
 
-    for font in (os.path.join("opensans", "OpenSans-Regular.ttf"),
-                 os.path.join("opensans", "OpenSans-Semibold.ttf"),
-                 os.path.join("fontawesome", "fontawesome-webfont.ttf")):
+    for font in (os.path.join("fontawesome", "fontawesome-webfont.ttf")):
         path = util.get_asset("font", font)
 
         # TODO(marcus): Check if they are already installed first.
